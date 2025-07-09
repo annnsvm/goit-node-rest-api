da5c37d0ea72da523093540668b18d9315631657
Anna Samoilova <samoilova0299@gmail.com>
2025-07-09 20:39:18 +0200

    hw3 done

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,15 +1,19 @@
 import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
-  id: Joi.string(),
   name: Joi.string().required(),
   email: Joi.string().required(),
   phone: Joi.string().required(),
+  favorite: Joi.boolean(),
 });
 
 export const updateContactSchema = Joi.object({
-  id: Joi.string(),
   name: Joi.string(),
   email: Joi.string(),
   phone: Joi.string(),
+  favorite: Joi.boolean(),
+});
+
+export const updateStatusSchema = Joi.object({
+  favorite: Joi.boolean().required(),
 });
